0bd72602ff428332f38540f932f4c10dc4b9e141
Kamil Preweda <preweda.kamil@o2.pl>
2019-07-23 18:15:57 +0200

    added js code

--- src/js/main.js
+++ src/js/main.js
@@ -3,11 +3,11 @@
 // service worker registration - remove if you're not going to use it
 
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
+  window.addEventListener('load', function () {
+    navigator.serviceWorker.register('serviceworker.js').then(function (registration) {
       // Registration was successful
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
+    }, function (err) {
       // registration failed :(
       console.log('ServiceWorker registration failed: ', err);
     });
@@ -16,7 +16,30 @@ if ('serviceWorker' in navigator) {
 
 // place your code below
 
+const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
 
-console.log(`Hello world!`);
+let lastChecked;
+
+function handleCheck(e) {
+  let inBetween = false;
+  // Check if they had the shift key down AND check that they are checking it
+  if (e.shiftKey && this.checked) {
+    checkboxes.forEach(checkbox => {
+      if (checkbox === this || checkbox === lastChecked) {
+        console.log(lastChecked);
+        inBetween = !inBetween;
+      }
+
+      if (inBetween) {
+        checkbox.checked = true;
+      }
+    })
+  }
+  lastChecked = this;
+
+
+}
+
+checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck));
 
 
